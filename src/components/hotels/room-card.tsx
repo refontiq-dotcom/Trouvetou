f1141410ab1c29f86d2fe6ede9902e9c5b5b63e7
refontiq-dotcom <refontiq@gmail.com>
2026-09-25 08:26:04 +0000

    fix: close listing card inner content wrapper

--- src/components/hotels/room-card.tsx
+++ src/components/hotels/room-card.tsx
@@ -271,6 +271,7 @@ export function RoomCard({ room, index = 0, priceSuffix = "par nuit" }: RoomCard
             </div>
           </div>
         </div>
+          </div>
       </motion.article>
 
       <AnimatePresence>
